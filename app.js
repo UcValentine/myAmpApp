4ada434f5c8dd3a0338b9c2c06f40ab93e544020
Valentine <ucval@yahoo.com>
2021-05-24 20:37:20 +0200

    conf entry file

--- app.js
+++ app.js
@@ -11,5 +11,29 @@ app.get('/', (_req, res) => {
             title: 'Home' 
         });
     });
+
+app.get('/about', (_req, res) => {
+        res.render('about', { 
+            title: 'About' 
+        });
+    });
+
+app.get('/blog', (_req, res) => {
+        res.render('blog', { 
+            title: 'Blog' 
+        });
+    });
+
+app.get('/gallary', (_req, res) => {
+        res.render('gallary', { 
+            title: 'Gallary' 
+        });
+    });
+
+app.get('/latest-blog', (_req, res) => {
+        res.render('latest-blog', { 
+            title: 'Latest Blogs' 
+        });
+    });
        
 app.listen(3000, () => console.log("Listening on port 3000"));
